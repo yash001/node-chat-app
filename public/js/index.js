0da6a709c77568be4e27ae979823a1e4e820c411
yash <mehtayash001@gmail.com>
2019-03-03 13:06:55 +0530

    Add css for chat page

--- public/js/index.js
+++ public/js/index.js
@@ -29,11 +29,13 @@ jQuery('#messages').append(li);
 jQuery('#message-form').on('submit', function (e) {
   e.preventDefault();
 
+  var messageTextbox =  jQuery('[name=message]');
+	
   socket.emit('createMessage', {
     from: 'User',
-    text: jQuery('[name=message]').val()
+    text: messageTextbox.val()
   }, function () {
-
+     messageTextbox.val('')
   });
 });
 
@@ -42,32 +44,18 @@ locationButton.on('click', function() {
 	if(!navigator.geolocation) {
 	  return alert('Geolocation not supported by you browser.');
 	}
+	
+	locationButton.attr('disabled', 'disabled').text('Sending location...');
 	navigator.geolocation.getCurrentPosition(function (position) {
 	//console.log(position); 	
+	locationButton.removeAttr('disabled').text('Send location');	
 	socket.emit('createLocationMessage',{
 	   latitude: position.coords.latitude,
 		longitude: position.coords.longitude
 	});	
 	},function () {
+	  locationButton.removeAttr('disabled').text('Send location');	
 	  alert('Unable to fetch location.');
 	});
 	
 });
-
-/*
-var locationButton = jQuery('#send-location');
-locationButton.on('click', function () {
-  if (!navigator.geolocation) {
-    return alert('Geolocation not supported by your browser.');
-  }
-
-  navigator.geolocation.getCurrentPosition(function (position) {
-    socket.emit('createLocationMessage', {
-      latitude: position.coords.latitude,
-      longitude: position.coords.longitude
-    });
-  }, function () {
-    alert('Unable to fetch location.');
-  });
-});
-*/
