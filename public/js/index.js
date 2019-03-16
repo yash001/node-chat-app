d481e45cac6975856e26ba63c9db38d8a2bd9b1f
yash <mehtayash001@gmail.com>
2019-03-16 16:25:59 +0530

    Add mustache.js for message templates

--- public/js/index.js
+++ public/js/index.js
@@ -10,23 +10,29 @@ socket.on('disconnect', function () {
 
 socket.on('newMessage', function (message) {
   var formattedTime = moment(message.createdAt).format('h:mm a');
-  //console.log('newMessage', message);
-  var li = jQuery('<li></li>');
-  li.text(`${message.from}:
-   ${formattedTime}: 
-   ${message.text}`);
-
-  jQuery('#messages').append(li);
+	var template = jQuery('#message-template').html();
+	
+ var html = Mustache.render(template,{
+   text: message.text,
+   from: message.from,
+   createdAt: formattedTime  
+ });	
+ jQuery('#messages').append(html);	
+	
 });
 
 socket.on('newLocationMessage',function(message){
  var formattedTime = moment(message.createdAt).format('h:mm a');
-var li = jQuery('<li></li>');
-var a = jQuery('<a target="_blank">My Current location</a>');
-li.text(`${message.from}: ${formattedTime}:`);
-a.attr('href',message.url);
-li.append(a);
-jQuery('#messages').append(li);
+ 
+var template = jQuery('#location-message-template').html();
+
+var html = Mustache.render(template, {
+   from: message.from,
+   url: message.url,
+   createdAt: formattedTime 	
+});
+
+jQuery('#messages').append(html);
 });
 
 jQuery('#message-form').on('submit', function (e) {
@@ -45,7 +51,7 @@ jQuery('#message-form').on('submit', function (e) {
 var locationButton = jQuery('#send-location');
 locationButton.on('click', function() {
 	if(!navigator.geolocation) {
-	  return alert('Geolocation not supported by you browser.');
+	  return alert('Geolocation not supported by your browser.');
 	}
 	
 	locationButton.attr('disabled', 'disabled').text('Sending location...');
--- public/playground/time.js
+++ public/playground/time.js
@@ -15,7 +15,7 @@ var moment = require('moment');
 
 
 var someTimestamp = moment().valueOf();
-console.log(someTimestamp)
+console.log(someTimestamp);
 
 var createdAt = 1234;
 var date = moment(createdAt); 
